4300520ddaeb23da2140258055e0bdbbcdc42c97
Lynn Hochendoner <lynn.hochendoner@gmail.com>
2021-12-03 09:40:51 -0500

    Setup repo

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.squareup.okio:okio:2.10.0")
+    testImplementation(kotlin("test"))
+}
