8f06fdab60307bd650c2b3adcbce7de86a7a50b0
Lynn Hochendoner <lynn.hochendoner@gmail.com>
2021-12-07 21:21:53 -0500

    Fix tests

--- src/test/kotlin/com/stilllynnthecloset/advent2021/Day07Test.kt
+++ src/test/kotlin/com/stilllynnthecloset/advent2021/Day07Test.kt
@@ -7,21 +7,21 @@ import kotlin.test.assertEquals
 class Day07Test {
     @Test
     fun exampleA() {
-        assertEquals(2 to 37, day07a(readFile("2021/day07example.txt")))
+        assertEquals(37, day07a(readFile("2021/day07example.txt")))
     }
 
     @Test
     fun testA() {
-        assertEquals(362 to 355150, day07a(readFile("2021/day07input.txt")))
+        assertEquals(355150, day07a(readFile("2021/day07input.txt")))
     }
 
     @Test
     fun exampleB() {
-        assertEquals(5 to 168, day07b(readFile("2021/day07example.txt")))
+        assertEquals(168, day07b(readFile("2021/day07example.txt")))
     }
 
     @Test
     fun testB() {
-        assertEquals(499 to 98368490, day07b(readFile("2021/day07input.txt")))
+        assertEquals(98368490, day07b(readFile("2021/day07input.txt")))
     }
 }
